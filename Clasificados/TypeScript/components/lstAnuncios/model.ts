cb1e6c6ba66226f89d0d7ce1004e53368762c114
rodrigo debian <correo.rodrigo@gmail.com>
2020-04-14 21:30:35 -0500

    busqueda y paginacion

---
+++ Clasificados/TypeScript/components/lstAnuncios/model.ts
@@ -0,0 +1,47 @@
+import { SearchData } from '../../models/searchData';
+import { ResultSet } from '../../models/resultSet';
+import { Api } from '../../shared/api';
+import { Model as PaginationModel } from '../pagination/model';
+import { Model as SearchFieldModel } from '../searchField/model';
+
+export class Model<T> {
+
+    public searchUrl: string;
+    public searchModel: SearchFieldModel;
+    public pagination: PaginationModel;
+    public ciudadId: KnockoutObservable<number>;
+    public list: KnockoutObservableArray<T>;    
+    private api: Api;
+
+    constructor(ko: KnockoutStatic, api: Api, searchUrl: string, ciudadId: number) {
+        this.api = api;
+        this.list = ko.observableArray<T>();
+        this.ciudadId = ko.observable<number>(ciudadId);
+        this.searchModel = new SearchFieldModel(ko);
+        this.pagination = new PaginationModel(ko, 1, [100]);
+        this.searchUrl = searchUrl;
+
+        this.pagination.page.subscribe(this.change);
+        this.pagination.pageSize.subscribe(this.change);
+        this.searchModel.searchText.subscribe(this.change);
+        this.change();
+    }
+
+    public change = async () => {
+        const self = this;
+        if (self.ciudadId() < 0) {
+            return;
+        }
+
+        let data: SearchData = {
+            ciudadId: self.ciudadId(),
+            limit: this.pagination.pageSize(),
+            offset: (this.pagination.page() - 1),
+            pattern: this.searchModel.searchText()
+        }
+
+        let set = await this.api.post<ResultSet<T[]>>(self.searchUrl, data);
+        this.pagination.totalRows(set.totalRows);
+        this.list(set.payload);
+    }
+}
--- Clasificados/TypeScript/pages/varios.ts
+++ Clasificados/TypeScript/pages/varios.ts
@@ -1,24 +1,33 @@
 import { View as LnkLugarView, Model as LnkLugarModel } from '../components/lnkLugar';
-import { View as ListVariosView, Model as ListVariosModel } from '../components/lstVarios';
+import { View as ListVariosView, Model as ListVariosModel } from '../components/lstAnuncios';
 import { Component } from '../services/component';
 import serverInfo from '../constants/serverInfo';
 import { JsonReq } from '../services/jsonReq';
 import { Redirect } from '../services/redirect';
+import { Varios } from '../models/varios';
+import urls from '../constants/serverInfo';
+
+import { View as PaginationView } from '../components/pagination';
+import { View as SearchFieldView } from '../components/searchField';
 
 $(() => {
-
     let api = new JsonReq(serverInfo.host, window);
     let url = new Redirect(window);
     let component = new Component(ko);
+
+    component.register("pagination", PaginationView, (params) => {
+        return params.model;
+    });
+    component.register("search-field", SearchFieldView, (params) => {
+        return params.model;
+    });
 
     component.register("lnk-lugar", LnkLugarView, (params) => {
         return new LnkLugarModel(ko, $, api, url);
     });
 
-    component.register("lst-varios", ListVariosView, (params) => {
-        let model = new ListVariosModel(ko, $, api);
-        model.init(params.ciudadId);
-        return model;
+    component.register("lst-varios", ListVariosView, (p) => {
+        return new ListVariosModel<Varios>(ko, api, urls.api.varios.search, p.ciudadId);       
     });
 
     ko.applyBindings();
